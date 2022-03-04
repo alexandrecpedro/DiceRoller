3c51837d2ee264334ece8e71a16e7a200a79d50b
alexandrecpedro <alexandrecpedro@gmail.com>
2022-03-04 16:47:59 -0300

    DiceRoller App

---
+++ README.md
@@ -0,0 +1,8 @@
+## Dice Roller
+
+Dice Roller is a simple app that rolls a six sided die.
+
+
+## Screenshots
+
+![Screenshot1](screenshots/screen0.png) ![Screenshot1](screenshots/screen1.png)
---
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.diceroller
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+
+/*
+DiceRoller is a simple Android app that rolls a six sided die.
+*/
+class MainActivity : AppCompatActivity() {
+
+    lateinit var diceImage: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Get the Button view from the layout and assign
+        val rollButton: Button = findViewById(R.id.roll_button)
+        // Calling the function (event onClick)
+        rollButton.setOnClickListener { rollDice() }
+
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    // Click listener for the Roll button
+    private fun rollDice() {
+        // Calling a Toast = message that pops up the screen for a short time
+        // Toast.makeText(stateOfApp, message, durationMessageAppear).show()
+        // Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
+        val randomInt = (1..6).random()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
